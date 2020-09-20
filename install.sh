c98f342a6c5d73e6e44855d39f5d51f2882c3163
Terence Broadbent <44728186+BroadbentT@users.noreply.github.com>
2020-09-20 22:03:04 +0100

    Small install file.

---
+++ install.sh
@@ -0,0 +1,25 @@
+cd /opt/
+apt-get install gnome-terminal
+apt-get install seclists
+apt-get install python3-pip
+apt-get install python3-ldap
+apt-get install gobuster
+apt-get install bloodhound
+gem install evil-winrm
+git clone https://github.com/SecureAuthCorp/impacket.git
+cd impacket/
+pip3 install .
+python3 setup.py install
+cd ..
+pip3 install kerbrute
+pip3 install aclpwn
+git clone https://github.com/ropnop/windapsearch.git
+
+
+
+
+
+
+
+
+
