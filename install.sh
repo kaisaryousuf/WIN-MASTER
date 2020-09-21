f830665b6aed447fd7e9e2e940017b026c97e3d2
Terence Broadbent <44728186+BroadbentT@users.noreply.github.com>
2020-09-21 14:54:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,17 @@
-cd /opt/
-apt-get install gnome-terminal
+cd /downloads/
 apt-get install seclists
+apt-get install gnome-terminal
 apt-get install python3-pip
 apt-get install python3-ldap
 apt-get install gobuster
-apt-get install bloodhound
 gem install evil-winrm
-git clone https://github.com/SecureAuthCorp/impacket.git
-cd impacket/
-pip3 install .
-python3 setup.py install
-cd ..
 pip3 install kerbrute
-pip3 install aclpwn
 git clone https://github.com/ropnop/windapsearch.git
+cd /windapsearch
+cp ./windapsearch.py ./usr/share/doc/python3-impacket/examples/windapsearch.py
+rm *.*
+cd ..
+rmdir /windapsearch
 
 
 
