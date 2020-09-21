f19dda193a451b53f6ce682e94d14d0e7c8d8c34
Terence Broadbent <44728186+BroadbentT@users.noreply.github.com>
2020-09-21 15:39:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-cd /downloads/
+cd ./downloads/
 apt-get install seclists
 apt-get install gnome-terminal
 apt-get install python3-pip
@@ -7,11 +7,11 @@ apt-get install gobuster
 gem install evil-winrm
 pip3 install kerbrute
 git clone https://github.com/ropnop/windapsearch.git
-cd /windapsearch
-cp /windapsearch.py /usr/share/doc/python3-impacket/examples/windapsearch.py
+cd ./windapsearch
+cp ./windapsearch.py ./usr/share/doc/python3-impacket/examples/windapsearch.py
 rm *.*
 cd ..
-rmdir /windapsearch
+rmdir ./windapsearch
 
 
 
