a25b373519a7bf3dcb00f55b3a46a75acf962ad3
Terence Broadbent <44728186+BroadbentT@users.noreply.github.com>
2020-11-15 12:44:53 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ apt-get install gobuster
 apt-get install crackmapexec
 gem install evil-winrm
 pip3 install kerbrute
+pip3 install smtp-user-enum
 git clone https://github.com/ropnop/windapsearch.git
 cd ./windapsearch
 mv ./windapsearch.py /usr/share/doc/python3-impacket/examples/windapsearch.py
