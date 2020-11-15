60314b8288536a3e91b8780b6e5449e9ce3dbe3a
root <root@kali.Domain>
2020-11-15 16:29:25 +0000

    Pr0J3CT_M@k30V3r

--- install.sh
+++ install.sh
@@ -1,15 +1,26 @@
-apt-get install seclists
-apt-get install gnome-terminal
-apt-get install python3-pip
-apt-get install python3-ldap
-apt-get install gobuster
-apt-get install crackmapexec
+#!/bin/bash
+# coding:UTF-8
+
+# -------------------------------------------------------------------------------------
+#                           WINDOWS MASTER PROGRAM INSTALL
+#                BY TERENCE BROADBENT BSC CYBER SECURITY (FIRST CLASS)
+# -------------------------------------------------------------------------------------
+
+apt-get install seclists -y
+apt-get install python3-pip -y
+apt-get install python3-ldap -y
+apt-get install gobuster -y
+apt-get install crackmapexec -y
 gem install evil-winrm
 pip3 install kerbrute
 pip3 install smtp-user-enum
+pip3 install termcolor 
 git clone https://github.com/ropnop/windapsearch.git
 cd ./windapsearch
 mv ./windapsearch.py /usr/share/doc/python3-impacket/examples/windapsearch.py
 cd ..
 rm -r windapsearch
-echo "All Done!"
+
+echo "I am all Done!!..."
+
+#Eof
